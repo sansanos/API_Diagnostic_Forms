f1c2e916e0b302b7b1d0c1471b05ed2901664957
sansanos <san-san-05@hotmail.com>
2025-01-10 10:43:06 -0500

    Commit 2

--- request.py
+++ request.py
@@ -2,15 +2,18 @@ import requests
 import configuration
 import data
 
-
+# Metodo POST para enviar formulario con datos validos
 def post_create_form_valid():
     return requests.post(configuration.URL_FORM + configuration.POST_PATH, json=data.post_body_valid, verify=False)
 
+# Metodo POST para enviar formulario con datos invalidos
 def post_create_form_invalid():
     return requests.post(configuration.URL_FORM + configuration.POST_PATH, json=data.post_body_invalid, verify=False)
 
+# Metodo POST para enviar formulario con 9 numeros en el campo Telefono 1
 def post_create_form_9_numbers():
     return requests.post(configuration.URL_FORM + configuration.POST_PATH, json=data.post_body_9_numbers, verify=False)
 
+# Metodo POST para enviar formulario con 11 numeros en el campo Telefono 1
 def post_create_form_11_numbers():
     return requests.post(configuration.URL_FORM + configuration.POST_PATH, json=data.post_body_11_numbers, verify=False)
--- test.py
+++ test.py
@@ -1,25 +1,31 @@
 import request
 
+# Funcion para validar codigo de respuesta exitosa
 def possitive_assert(response):
     code = response.status_code
     assert code == 200
 
+# 1 Validacion formulario con datos validos
 def test_post_valid():
     response = request.post_create_form_valid()
     possitive_assert(response)
 
+# 2 Validacion campo Telefono 1 con 9 numeros
 def test_post_9_numbers():
     response = request.post_create_form_9_numbers()
     possitive_assert(response)
 
+# Funcion para validar codigo de respuesta fallida
 def negative_assert(response):
     code = response.status_code
     assert code == 422
 
+# 4 Validacion formulario con datos invalidos
 def test_post_invalid():
     response = request.post_create_form_invalid()
     negative_assert(response)
 
+# 5 Validacion campo Telefono 1 con 11 numeros
 def test_post_11_numbers():
     response = request.post_create_form_11_numbers()
     negative_assert(response)
